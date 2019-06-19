bc8c19e6b53bd2a86e4e2aaf6614e79fb8eaeaf4
Resham Arora <resham.arora@gmail.com>
2019-06-19 02:12:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,10 +68,7 @@ def adding_matz
 # }
 
 	programmer_hash = 
- 		{    :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
- 		  
+ 		{
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -86,7 +83,8 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+programmer_hash
 end
 
 def changing_alan
